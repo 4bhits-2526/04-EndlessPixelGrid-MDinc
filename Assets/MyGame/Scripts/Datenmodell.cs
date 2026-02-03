5afc1f512666d7b994188074ee8d40abffd2f6ac
mdinchtl <medine.dinc07@htl-salzburg.ac.at>
2026-02-03 14:50:48 +0100

    implement toggle logic for input line pixels

--- Assets/MyGame/Scripts/Datenmodell.cs
+++ Assets/MyGame/Scripts/Datenmodell.cs
@@ -1,19 +1,97 @@
 using UnityEngine;
 
 public class Datenmodell : MonoBehaviour
-{
-    public bool[] Eingabezeile = new bool[7];
+{   
+    public bool[] Eingabezeilen = new bool[7];
     public bool[,] Raster = new bool[10, 7];
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (Eingabezeilen[0] == true)
+            {
+                Eingabezeilen[0] = false;
+            }
+            else if (Eingabezeilen[0] == false)
+            {
+                Eingabezeilen[0] = true;
+            }
+        }
+ 
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (Eingabezeilen[1] == true)
+            {
+                Eingabezeilen[1] = false;
+            }
+            else if (Eingabezeilen[1] == false)
+            {
+                Eingabezeilen[1] = true;
+            }
+        }
+ 
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (Eingabezeilen[2] == true)
+            {
+                Eingabezeilen[2] = false;
+            }
+            else if (Eingabezeilen[2] == false)
+            {
+                Eingabezeilen[2] = true;
+            }
+        }
+ 
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (Eingabezeilen[3] == true)
+            {
+                Eingabezeilen[3] = false;
+            }
+            else if (Eingabezeilen[3] == false)
+            {
+                Eingabezeilen[3] = true;
+            }
+        }
+ 
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (Eingabezeilen[4] == true)
+            {
+                Eingabezeilen[4] = false;
+            }
+            else if (Eingabezeilen[4] == false)
+            {
+                Eingabezeilen[4] = true;
+            }
+        }
+ 
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (Eingabezeilen[5] == true)
+            {
+                Eingabezeilen[5] = false;
+            }
+            else if (Eingabezeilen[5] == false)
+            {
+                Eingabezeilen[5] = true;
+            }
+        }
+ 
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (Eingabezeilen[6] == true)
+            {
+                Eingabezeilen[6] = false;
+            }
+            else if (Eingabezeilen[6] == false)
+            {
+                Eingabezeilen[6] = true;
+            }
+        }
+ 
     }
+ 
+    
 }
