d110d25635e29cb21adcc15201f140fb41df10c5
mdinchtl <medine.dinc07@htl-salzburg.ac.at>
2026-02-03 15:08:01 +0100

    apply fifo shift to grid data on commit

--- Assets/MyGame/Scripts/Datenmodell.cs
+++ Assets/MyGame/Scripts/Datenmodell.cs
@@ -1,107 +1,60 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Datenmodell : MonoBehaviour
-{   
+{
+   
     public bool[] Eingabezeilen = new bool[7];
     public bool[,] Raster = new bool[10, 7];
 
+    
+    public Image[] inputLineImages;      // 7 Stück
+    public Image[] gridImagesFlat;       // 70 Stück (oben links → unten rechts)
+
+    private Image[,] gridImages = new Image[10, 7];
+    void Start()
+    {
+        // Flat-Array (70) in 2D-Array (10×7) umwandeln
+        int index = 0;
+        for (int row = 0; row < 10; row++)
+        {
+            for (int col = 0; col < 7; col++)
+            {
+                gridImages[row, col] = gridImagesFlat[index];
+                index++;
+            }
+        }
+
+        RenderAll();
+    }
+
+    
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.D))
         {
-                ApplyFifoShift();
+            ApplyFifoShift();
+            RenderAll();
         }
 
-
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            if (Eingabezeilen[0] == true)
-            {
-                Eingabezeilen[0] = false;
-            }
-            else if (Eingabezeilen[0] == false)
-            {
-                Eingabezeilen[0] = true;
-            }
-        }
- 
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            if (Eingabezeilen[1] == true)
-            {
-                Eingabezeilen[1] = false;
-            }
-            else if (Eingabezeilen[1] == false)
-            {
-                Eingabezeilen[1] = true;
-            }
-        }
- 
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            if (Eingabezeilen[2] == true)
-            {
-                Eingabezeilen[2] = false;
-            }
-            else if (Eingabezeilen[2] == false)
-            {
-                Eingabezeilen[2] = true;
-            }
-        }
- 
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            if (Eingabezeilen[3] == true)
-            {
-                Eingabezeilen[3] = false;
-            }
-            else if (Eingabezeilen[3] == false)
-            {
-                Eingabezeilen[3] = true;
-            }
-        }
- 
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            if (Eingabezeilen[4] == true)
-            {
-                Eingabezeilen[4] = false;
-            }
-            else if (Eingabezeilen[4] == false)
-            {
-                Eingabezeilen[4] = true;
-            }
-        }
- 
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            if (Eingabezeilen[5] == true)
-            {
-                Eingabezeilen[5] = false;
-            }
-            else if (Eingabezeilen[5] == false)
-            {
-                Eingabezeilen[5] = true;
-            }
-        }
- 
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            if (Eingabezeilen[6] == true)
-            {
-                Eingabezeilen[6] = false;
-            }
-            else if (Eingabezeilen[6] == false)
-            {
-                Eingabezeilen[6] = true;
-            }
-        }
- 
+        if (Input.GetKeyDown(KeyCode.W)) ToggleInput(0);
+        if (Input.GetKeyDown(KeyCode.A)) ToggleInput(1);
+        if (Input.GetKeyDown(KeyCode.UpArrow)) ToggleInput(2);
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) ToggleInput(3);
+        if (Input.GetKeyDown(KeyCode.DownArrow)) ToggleInput(4);
+        if (Input.GetKeyDown(KeyCode.RightArrow)) ToggleInput(5);
+        if (Input.GetKeyDown(KeyCode.S)) ToggleInput(6);
     }
 
-    void ApplyFifoShift()
+    void ToggleInput(int index)
     {
-    // 1. Raster nach oben schieben
+        Eingabezeilen[index] = !Eingabezeilen[index];
+        RenderInputLine();
+    }
+
+        void ApplyFifoShift()
+    {
+        // Raster nach oben schieben
         for (int row = 0; row < 9; row++)
         {
             for (int col = 0; col < 7; col++)
@@ -110,19 +63,43 @@ public class Datenmodell : MonoBehaviour
             }
         }
 
-        // 2. Eingabezeile unten einfügen (Zeile 9)
+        // Eingabezeile unten einfügen
         for (int col = 0; col < 7; col++)
         {
             Raster[9, col] = Eingabezeilen[col];
         }
 
-        // 3. Eingabezeile zurücksetzen
+        // Eingabezeile zurücksetzen
         for (int i = 0; i < 7; i++)
         {
             Eingabezeilen[i] = false;
         }
     }
 
- 
-    
+    void RenderAll()
+    {
+        RenderGrid();
+        RenderInputLine();
+    }
+
+    void RenderGrid()
+    {
+        for (int row = 0; row < 10; row++)
+        {
+            for (int col = 0; col < 7; col++)
+            {
+                gridImages[row, col].color =
+                    Raster[row, col] ? Color.white : Color.black;
+            }
+        }
+    }
+
+    void RenderInputLine()
+    {
+        for (int i = 0; i < 7; i++)
+        {
+            inputLineImages[i].color =
+                Eingabezeilen[i] ? Color.white : Color.black;
+        }
+    }
 }
