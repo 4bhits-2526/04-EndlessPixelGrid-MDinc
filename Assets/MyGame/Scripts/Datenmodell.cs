363fddc51399427e54c89b2988bc9395bfe954ad
mdinchtl <medine.dinc07@htl-salzburg.ac.at>
2026-02-03 14:43:59 +0100

    add data modell for grid and input line

---
+++ Assets/MyGame/Scripts/Datenmodell.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Datenmodell : MonoBehaviour
+{
+    public bool[] Eingabezeile = new bool[7];
+    public bool[,] Raster = new bool[10, 7];
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
